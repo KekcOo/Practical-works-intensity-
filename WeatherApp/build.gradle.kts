b7bc28785ccb302768b17728e3fc6ea07fd00c98
KekcOo <157485582+KekcOo@users.noreply.github.com>
2025-02-26 08:54:54 +0300

    add hibernate.cfg.xml

--- WeatherApp/build.gradle.kts
+++ WeatherApp/build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     implementation("org.jsoup:jsoup:1.16.1")
     compileOnly("org.projectlombok:lombok:1.18.36")
     annotationProcessor("org.projectlombok:lombok:1.18.36")
+    implementation("org.hibernate:hibernate-core:6.5.2.Final")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.google.code.gson:gson:2.11.0")
     implementation( "org.apache.logging.log4j:log4j-slf4j-impl:2.17.2" )
--- WeatherApp/src/main/java/com/example/Main.java
+++ WeatherApp/src/main/java/com/example/Main.java
@@ -1,28 +1,16 @@
 package com.example;
 
 import com.example.itil.InitDb;
-import com.example.itil.WeatherApp;
+
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Scanner;
 @Slf4j
 public class Main {
     public static void main(String[] args) {
-
-
         InitDb.createTable();
 
-        Scanner scanner = new Scanner(System.in);
 
-        String input = "";
-        log.info("Введите город ");
-        log.info("Пример: Москва ");
 
-        while (!input.equals("exit")) {
-            input = scanner.nextLine().toLowerCase().trim();
-            WeatherApp.getWeather(input);
-
-        }
 
     }
 }
